e70cde76132e3350b32842ea016b82df2f75dc3e
fastwings <sivan@time-tunnel.co.il>
2012-06-01 23:33:25 +0300

    Rdy for dotcloud

--- js/libs/Portfolio/Exceptions/ApplicationException.js
+++
@@ -1,8 +0,0 @@
-window.Portfolio.ApplicationExcption  = function (message) {
-	this.message = message;
-	_logat();
-	function _logat() {
-		console.error(message);
-	}
-}
-Portfolio.ApplicationExcption.prototype = new Error;
--- js/libs/Portfolio/Exceptions/SceneException.js
+++
@@ -1,9 +0,0 @@
-window.Portfolio.SceneExcption  = function (message,module,subModule) {
-	this.message = message;
-	_logat();
-	function _logat() {
-		console.info("Error Happend on Module: %s , On SubModule: %s");
-		console.error(message);
-	}
-}
-Portfolio.SceneExcption.prototype = new Error; 
